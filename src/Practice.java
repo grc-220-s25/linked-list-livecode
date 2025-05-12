af4f2b2586f4206af69f91ca648da93bf868bebb
ryanrmills <ryanroymills@gmail.com>
2025-05-12 11:37:01 -0700

    livecode commit 1

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,24 @@
 public class Practice {
     public static void main(String[] args) {
-        
+        ListNode head = new ListNode(4);
+        ListNode mySeven = new ListNode(7);
+
+        head.next = mySeven;
+
+        mySeven.next = new ListNode(28);
+        head.next.next.next = new ListNode(32);
+        head.next.next.next.next = new ListNode(23);
+
+        System.out.println(head.data);
+        System.out.println(head.next.data);
+
+        ListNode current = head;
+
+        while (current != null){
+            System.out.println(current.data);
+
+            current = current.next;
+        }
+
     }
 }
