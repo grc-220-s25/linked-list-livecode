ac648ddf6cfb7e27840f160e7dbc44677bf0a96f
Bluenzo <80460870+Bluenzo@users.noreply.github.com>
2025-05-27 23:28:52 -0700

    Livecode

--- src/ListNode.java
+++ src/ListNode.java
@@ -1,3 +1,13 @@
 public class ListNode {
+
+    //
+    public int data; 
+    public ListNode next;
+
+    //constructor
+    public ListNode(int data){
+        this.data = data;
+    }
+
     
 }
